9bfea9754b28ac813c6422562a1141bc5b029232
Andriikryl <andreyk557@gmail.com>
2023-11-02 17:25:56 +0200

    add visualyHidden component

---
+++ src/components/modal/Madal.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import { createPortal } from "react-dom";
+import { X as Close } from "react-feather";
+import FocusLock from "react-focus-lock";
+import { RemoveScroll } from "react-remove-scroll";
+import VisuallyHidden from "../visuallyhidden/VisuallyHidden";
+import styles from "./Modal.module.css";
+
+export default function Madal() {
+  return <div>Madal</div>;
+}
